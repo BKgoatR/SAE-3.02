d5c888d20f90460b3b46a2c2089bc6fe572a484c
vnbkr <vn.bkr67230@gmail.com>
2026-09-22 14:19:09 +0200

    azz

--- src/main.py
+++ src/main.py
@@ -17,7 +17,6 @@ class App(QMainWindow):
 
             Vehicule(100, 400, direction="droite", prioritaire=False),
 
-
             Vehicule(350, 100, direction="bas", prioritaire=False)
         ]
 
